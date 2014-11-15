0499823e75435a6308f15ae3fa216283f533e773
joffroy59 <jerome.offroy@gmail.com>
2014-11-15 12:49:04 +0100

    change master

--- testSubModuleA/src/main/java/testSubModuleA/Application.java
+++ testSubModuleA/src/main/java/testSubModuleA/Application.java
@@ -3,7 +3,7 @@ package testSubModuleA;
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World A !!");
+		System.out.println("Hello World A master modif!!");
 	}
 
 }
