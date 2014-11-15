9fdf4bf7482a00a6964cf3018fb566486591c88c
joffroy59 <jerome.offroy@gmail.com>
2014-11-15 13:41:25 +0100

    add 3rd line

--- testSubModuleA/src/main/java/testSubModuleA/Application.java
+++ testSubModuleA/src/main/java/testSubModuleA/Application.java
@@ -5,6 +5,7 @@ public class Application {
 	public static void main(String[] args) {
 		System.out.println("Hello World A (evo1)!!");
 		System.out.println("Hello World A (evo2)!!");
+		System.out.println("Hello World A your_branch!!");
 	}
 
 }
